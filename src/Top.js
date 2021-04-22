4ba9abe8c215d5c50f43ea86302176d3fb1d90ae
Emma Lynn <emmalynnn97@gmail.com>
2021-04-21 17:01:10 -0700

    update top responsiveness

--- src/Top.js
+++ src/Top.js
@@ -1,23 +1,69 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import me from './me.png'
 const Top = () => {
+    const [spanColor, setColor] = useState('inherit')
     const outerStyle={
         height:'75vh',
         width:'80%',
         background:'#FFEDFB',
-        float:'right'
+        float:'right',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'flex-start',
+        paddingLeft:96,
+        paddingRight:96
     }
+    const coloredSpan={
+        color:spanColor,
+        transition:'.15s ease-in'
+    }
+    const imgStyle={
+        position:'absolute',
+        borderRadius:'50%',
+      
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            setColor('#FF65DE')
+        })
+    }, [])
     return (
-        <div style={outerStyle}>
+        <div className='top-outer' style={outerStyle}>
+            <img style={imgStyle} alt="me" src={me}/>
             <div>
-                <span>Hi, I’m Emma Lynn</span>
-                <h1>I use development and design to convert ideas into digital experiences</h1>
-                <p>
+                <span style={{fontSize:22}}>Hi, I’m <span style={coloredSpan}>Emma Lynn</span>
+                </span>
+                <h1>I use <span style={coloredSpan}>development</span> and <span style={coloredSpan}>design</span> to convert <span style={coloredSpan}>ideas</span> into <span style={coloredSpan}>digital experiences</span></h1>
+                <p style={{
+                    marginBottom:15,
+                    }}>
                   I am an Arizona based Software Engineer and UI/UX Designer who specializes in the development of beautiful user interfaces and websites
                 </p>
                 <div>
-                    <button>Contact</button>
-                    <button>Portfolio</button>
+                    <button style={{
+                        fontSize:24,
+                        marginRight:20,
+                        padding:'.25em 1em',
+                        background:'#FF65DE',
+                        color:'white',
+                        outline:'none',
+                        border:'none',
+                        cursor:'pointer',
+                        fontWeight:700
+                        }}>Contact</button>
+                    <button style={{
+                        fontSize:24,
+                        marginRight:20,
+                        padding:'.25em 1em',
+                        background:'white',
+                        color:'#FF65DE',
+                        outline:'none',
+                        border:'none',
+                        cursor:'pointer',
+                        fontWeight:700
+                        }}>Portfolio</button>
                 </div>
             </div>
         </div>
